7f7155d5103f4b5b1595faac6c10941de5f5588c
smitapawar0195 <smitapawar0195@gmail.com>
2024-05-10 16:40:23 +0530

    First testcase by smita

---
+++ src/test/java/Com/test/TestSmita.java
@@ -0,0 +1,15 @@
+package Com.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestSmita {
+
+	@Test
+	public void m2() {
+		ChromeDriver d = new ChromeDriver();
+		d.get("https://byjus.com/ias-questions/what-is-para-in-banking/");
+
+	}
+
+}
